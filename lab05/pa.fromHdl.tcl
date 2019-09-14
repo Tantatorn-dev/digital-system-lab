3545e8beda071f0c4922d6f565c3f26064fae4d9
Tantatorn <timefear007@gmail.com>
2019-09-14 23:18:56 +0700

    add BCDto7Seg in lab05

--- lab05/pa.fromHdl.tcl
+++ lab05/pa.fromHdl.tcl
@@ -1,13 +1,13 @@
 
 # PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
 
-create_project -name lab05 -dir "/home/lord-tantatorn/digital-system-lab/lab05/planAhead_run_1" -part xc6slx9tqg144-3
+create_project -name lab05 -dir "/home/lord-tantatorn/digital-system-lab/lab05/planAhead_run_3" -part xc6slx9tqg144-3
 set_param project.pinAheadLayout yes
 set srcset [get_property srcset [current_run -impl]]
-set_property target_constrs_file "JK_FlipFlop.ucf" [current_fileset -constrset]
-set hdlfile [add_files [list {JK_FlipFlop.vhd}]]
+set_property target_constrs_file "FourBitsToSevenSegment.ucf" [current_fileset -constrset]
+set hdlfile [add_files [list {FourBitsToSevenSegment.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
-set_property top JK_FlipFlop $srcset
-add_files [list {JK_FlipFlop.ucf}] -fileset [get_property constrset [current_run]]
+set_property top FourBitsToSevenSegment $srcset
+add_files [list {FourBitsToSevenSegment.ucf}] -fileset [get_property constrset [current_run]]
 open_rtl_design -part xc6slx9tqg144-3
